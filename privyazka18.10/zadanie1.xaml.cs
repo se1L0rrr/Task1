777a93d81cdfe4074908dccf1a76a9aee3d6033a
se1L0rrr <idrisovhasan@mail.ru>
2024-10-20 18:48:39 +0500

    dobavil privyazky

---
+++ privyazka18.10/zadanie1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp25
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            string name = nameTextBox.Text;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Имя не может быть пустым!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (name.Any(c => c >='А' && c <= 'я'))
+            {
+                MessageBox.Show("Введите имя на английском");
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели: " + name, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+    }
+}
---
+++ privyazka18.10/zadanie2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            textBox.FontSize = e.NewValue;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = "Сохранить текст"
+            };
+
+            if (saveFile.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFile.FileName, textBox.Text);
+            }
+        }
+    }
+}
